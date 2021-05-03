4e133770f1a52e6fa8697c99a662576c4adab217
1Papillon1 <83593078+1Papillon1@users.noreply.github.com>
2021-05-03 15:54:02 +0200

    Add files via upload

---
+++ Pizzeria Moscato/js/orders.js
@@ -0,0 +1,58 @@
+let price = 0;
+let total = 0;
+
+const addCapricciosa = () => {
+    let x = document.getElementById('total');
+    price = 12.99;
+    total += price;
+    x.innerHTML = 'Total: <br>$' + total.toFixed(2);
+}
+
+const addMargharita = () => {
+    let x = document.getElementById('total');
+    price = 8.99;
+    total += price;
+    x.innerHTML = 'Total: <br>$' + total.toFixed(2);
+}
+
+const addHawaiian = () => {
+    let x = document.getElementById('total');
+    price = 6.99;
+    total += price;
+    x.innerHTML = 'Total: <br>$' + total.toFixed(2);
+}
+
+const addVegeterian = () => {
+    let x = document.getElementById('total');
+    price = 13.49;
+    total += price;
+    x.innerHTML = 'Total: <br>$' + total.toFixed(2);
+}
+
+const addMulberry = () => {
+    let x = document.getElementById('total');
+    price = 12.99;
+    total += price;
+    x.innerHTML = 'Total: <br>$' + total.toFixed(2);
+}
+
+const addRusticaChicken= () => {
+    let x = document.getElementById('total');
+    price = 15.00;
+    total += price;
+    x.innerHTML = 'Total: <br>$' + total.toFixed(2);
+}
+
+const addMeditarranean = () => {
+    let x = document.getElementById('total');
+    price = 16.50;
+    total += price;
+    x.innerHTML = 'Total: <br>$' + total.toFixed(2);
+}
+
+const addBBQChicken = () => {
+    let x = document.getElementById('total');
+    price = 16.00;
+    total += price;
+    x.innerHTML = 'Total: <br>$' + total.toFixed(2);
+}
